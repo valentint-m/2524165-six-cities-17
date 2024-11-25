02560593b5f5bf075de55f4d2ddd20345d16fc2a
Valentin Tazhetdinov <valentyntm@gmail.com>
2024-11-25 21:16:17 +0300

    Добавит маршрутизацию.

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,13 @@
-export const Option = {
+const Option = {
   OffersCount: 4
-};
+} as const;
+
+const Path = {
+  Main: '/',
+  Login: '/login',
+  Favorites: '/favorites',
+  Offer: '/offer/:id'
+} as const;
+
+export { Option, Path };
+
