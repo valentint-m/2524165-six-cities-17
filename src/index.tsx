a7aa124a8828d7a4dda1ed009816483a5ed3d8bf
Valentin Tazhetdinov <valentyntm@gmail.com>
2024-12-23 19:56:59 +0300

    Подключит thunk и передаст ему экземпляр axios.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import { store } from './store';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import { store } from './store';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
