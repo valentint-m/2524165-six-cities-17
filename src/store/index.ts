f40e4c12099fe26296e23199edc1ba31a29aaf97
Valentin Tazhetdinov <valentyntm@gmail.com>
2025-01-19 16:10:27 +0300

    Подключит новый редьюсер.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
+import { rootReducer } from './root-reducer';
 
 const api = createAPI();
 
 export const store = configureStore({
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       thunk: {
